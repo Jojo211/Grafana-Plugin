f98b060e377eddf0d7299db942328a874aef8428
Jojo211 <44054870+Jojo211@users.noreply.github.com>
2018-10-12 11:21:30 +0200

    Add files via upload

--- src/module.js
+++ src/module.js
@@ -2,8 +2,9 @@ import { graph } from './graph';
 import { loadPluginCss } from 'app/plugins/sdk';
 
 loadPluginCss({
-  dark: 'plugins/piechart-panel-master/src/css/exemples.dark.css',
-  light: 'plugins/piechart-panel-master/src/css/exemples.light.css',
+	dark: 'plugins\jojo-checkbox-panel\src\css',
+  //dark: 'plugins/piechart-panel-master/src/css/exemples.dark.css',
+  //light: 'plugins/piechart-panel-master/src/css/exemples.light.css',
 });
 
 export { graph as PanelCtrl };
